69737dbf8fdb1f2f2af30650ea004bba22f6c83f
Max <maksymkharuk@iesebre.com>
2015-11-29 17:53:20 +0100

    Added routing-controllers-views exercise

---
+++ routing-controllers-views/learning-laravel-5/app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class WelcomeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //return 'hello world!';
+
+        return view('welcome');
+    }
+
+    public function contact()
+    {
+        return view('pages.contact');
+    }
+}
